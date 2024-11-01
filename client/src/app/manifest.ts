1c8f4a611922661e5e214adc9fa7901d62d2ab4b
alideweb <alimoradi0business@gmail.com>
2024-11-01 12:26:59 +0330

    manifest added

---
+++ client/src/app/manifest.ts
@@ -0,0 +1,44 @@
+import type { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "Ekka Shop",
+    short_name: "Ekka Shop",
+    start_url: "/",
+    scope: "/",
+    display: "standalone",
+    theme_color: "#3474d4",
+    background_color: "#3474d4",
+    icons: [
+      {
+        sizes: "64x64",
+        src: "/PWA/icon64.png",
+        type: "image/png",
+      },
+      {
+        sizes: "128x128",
+        src: "/PWA/icon128.png",
+        type: "image/png",
+      },
+      {
+        sizes: "512x512",
+        src: "/PWA/icon512.png",
+        type: "image/png",
+      },
+    ],
+    screenshots: [
+      {
+        src: "/PWA/screenshot-wide.png",
+        form_factor: "wide",
+        type: "image/png",
+        sizes: "1902x872",
+      },
+      {
+        src: "/PWA/screenshot-narrow.png",
+        form_factor: "narrow",
+        type: "image/png",
+        sizes: "312x671",
+      },
+    ],
+  };
+}
